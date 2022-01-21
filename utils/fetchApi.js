92f6039bf3ff2b0f4edfeb3183fa6ba7dfc67026
yuashi <singh9ayushi9@gmail.com>
2022-01-21 20:44:09 +0530

    fixed2

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async (url) => {
   const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": process.env.RAPID_API_KEY,
+      "x-rapidapi-key": "4a5cc6bfcfmsh3ac860ba82415ccp164ea1jsn1d227b0e6ba1",
     },
   });
   return data;
